e7a265fa23fc75d890a0bbbc6df36857aa961768
kamilcaglar <alarkamil34@gmail.com>
2022-10-11 20:23:56 +0200

    first commit

---
+++ TestApp/SelfSizingTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SelfSizingTableViewCell.swift
+//  TestApp
+//
+//  Created by Kamil Caglar on 11/10/2022.
+//
+
+import UIKit
+
+class SelfSizingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellLabelText: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TestApp/ViewController.swift
@@ -0,0 +1,163 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Kamil Caglar on 10/10/2022.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var people: [NSManagedObject] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "The List"
+        //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+     
+        tableView.rowHeight = UITableView.automaticDimension
+//        tableView.rowHeight = UITableView.automaticDimension
+//        tableView.estimatedRowHeight = 300
+
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      
+      //1
+      guard let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate else {
+          return
+      }
+      
+      let managedContext =
+        appDelegate.persistentContainer.viewContext
+      
+      //2
+        
+      let fetchRequest =
+        NSFetchRequest<NSManagedObject>(entityName: "Person")
+      
+      //3
+      do {
+        people = try managedContext.fetch(fetchRequest)
+      } catch let error as NSError {
+        print("Could not fetch. \(error), \(error.userInfo)")
+      }
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard let appDelegate =
+                    UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            
+            // 1
+            let managedContext =
+            appDelegate.persistentContainer.viewContext
+            
+            // Remove the people from the CoreData
+            appDelegate.persistentContainer.viewContext.delete(self.people[indexPath.row])
+            //self.people.remove(at: indexPath.row)
+            // Save Changes
+            do {
+                try managedContext.save()
+                //self.people.remove(at: indexPath.row)
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+            // Remove row from TableView
+            //self.tableView.deleteRows(at: [indexPath], with: .fade)
+            self.people.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+    }
+
+
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Add a new name",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            guard let textField = alert.textFields?.first,
+                  let nameToSave = textField.text else {
+                return
+            }
+            self.save(name: nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    func save(name: String) {
+        
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        // 1
+        let managedContext =
+        appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+        NSEntityDescription.entity(forEntityName: "Person",
+                                   in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        person.setValue(name, forKeyPath: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return people.count
+  }
+
+  func tableView(_ tableView: UITableView,cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let person = people[indexPath.row]
+      let selfSizingCell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SelfSizingTableViewCell
+      selfSizingCell.cellLabelText.text = person.value(forKey: "name") as? String
+      selfSizingCell.cellLabelText?.numberOfLines = 0
+//    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//      cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+//    return cell
+      return selfSizingCell
+  }
+
+}
+
+
+
